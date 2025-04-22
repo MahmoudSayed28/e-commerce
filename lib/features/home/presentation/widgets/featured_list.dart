2c04143997dce9aa0be320b5b9be5c32dc70cf67
mahmoud <mamhouds22@gmail.com>
2025-04-22 16:07:45 +0200

    FeaturedList

---
+++ lib/features/home/presentation/widgets/featured_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_app/features/home/presentation/widgets/featured_item.dart';
+
+class FeaturedList extends StatelessWidget {
+  const FeaturedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.25,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(children: List.generate(4, (index) => const FeaturedItem())),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fruits_app/features/home/presentation/widgets/custom_home_appbar.dart';
 import 'package:fruits_app/features/home/presentation/widgets/featured_item.dart';
+import 'package:fruits_app/features/home/presentation/widgets/featured_list.dart';
 import 'package:fruits_app/features/home/presentation/widgets/home_search_textfield.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -16,7 +17,7 @@ class HomeViewBody extends StatelessWidget {
         slivers: [
           CustomHomeAppbar(),
           SliverToBoxAdapter(child: HomeSearchTextfield()),
-          SliverToBoxAdapter(child: FeaturedItem()),
+          SliverToBoxAdapter(child: FeaturedList()),
         ],
       ),
     );
