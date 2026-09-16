bc62965def32d64105fe779bcc80a0097bb1b3db
MahmoudSayed28 <mamhouds22@gmail.com>
2026-09-16 18:17:24 +0300

    remove unused tools

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
- // await SupabaseHelper.setUpSupabase();
 
   initServiceLocator();
   await CacheHelper.init();
